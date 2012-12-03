da572bb835f0356953726f8c6131269edac301b1
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-12-03 00:47:40 -0600

    Sync DJs via member API (issue #2, issue #3)

---
+++ auth/tasks.py
@@ -0,0 +1,70 @@
+import logging
+
+from django import http
+from django.utils import simplejson as json
+
+from auth import roles
+from auth.models import User
+
+log = logging.getLogger()
+
+
+def sync_user(request):
+    user = request.POST.get('user')
+    if not user:
+        return http.HttpResponseBadRequest()
+    user = json.loads(user)
+
+    qs = User.all().filter('external_id =', user['member_id'])
+    users = qs.fetch(1)
+    dj_user = None
+    if len(users):
+        dj_user = users[0]
+    else:
+        # No previously sync'd user exists.
+        # Let's check by email to see if an old
+        # user exists with the same email.
+        qs = (User.all().filter('email =', user['email'])
+                        .filter('external_id =', None))
+        users = qs.fetch(1)
+        if len(users):
+            log.info('Linking user %s to ID %s' %
+                     (user['email'], user['member_id']))
+            dj_user = users[0]
+
+    fields = {
+        'first_name': user['name_first'],
+        'last_name': user['name_last'],
+        'email': user['email'],
+        'dj_name': user['nick'],
+        'external_id': user['member_id'],
+        'is_superuser': False,
+        'is_active': True,
+        'roles': [roles.DJ],
+    }
+    if not dj_user:
+        dj_user = User(**fields)
+    else:
+        for k, v in fields.items():
+            setattr(dj_user, k, v)
+    dj_user.put()
+
+    return http.HttpResponse('OK')
+
+
+def deactivate_user(request):
+    id = request.POST.get('external_id')
+    if not id:
+        log.info('external_id not found in POST')
+        return http.HttpResponseBadRequest()
+    qs = User.all().filter('external_id =', int(id))
+    users = qs.fetch(1)
+    if not len(users):
+        return http.HttpResponseBadRequest()
+
+    dj_user = users[0]
+    dj_user.is_active = False
+    dj_user.put()
+    log.info('Deactivated user %s %s' % (dj_user, dj_user.email))
+
+    return http.HttpResponse('OK')
